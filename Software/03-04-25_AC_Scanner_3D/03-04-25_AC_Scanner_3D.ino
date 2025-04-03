10dd31bb92676d45d21bb1704eab5c5a9b633a04
Alexandros Chr <alexandremchrissantakis@gmail.com>
2025-04-03 09:31:52 +0200

    Ajout des Include
    Inclusion des librairie tmc , wire et VLX

--- Software/03-04-25_AC_Scanner_3D/03-04-25_AC_Scanner_3D.ino
+++ Software/03-04-25_AC_Scanner_3D/03-04-25_AC_Scanner_3D.ino
@@ -3,6 +3,11 @@
 //************************* Arduino IDE **************************
 //***************** M5Stack, TMC5072-BOB, VL53L0X ****************
 
+//**Include**
+#include "TMC5072.h"
+#include <Wire.h>
+#include <VL53L0X.h>
+
 
 void setup() {
   // put your setup code here, to run once:
