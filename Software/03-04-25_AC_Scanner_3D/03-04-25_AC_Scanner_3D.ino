58f657289393f64356aed693f600e908d4cdd58d
Alexandros Chr <alexandremchrissantakis@gmail.com>
2025-04-03 09:07:29 +0200

    Création du code
    Création du code, avec une entête

---
+++ Software/03-04-25_AC_Scanner_3D/03-04-25_AC_Scanner_3D.ino
@@ -0,0 +1,15 @@
+//******************* Alexandros Chrissantakis *******************
+//************************* 2024 - 2025 **************************
+//************************* Arduino IDE **************************
+//***************** M5Stack, TMC5072-BOB, VL53L0X ****************
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
